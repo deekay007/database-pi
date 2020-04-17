f6b1e6298dbf2d974e7822b63dd5c14251c0d07f
Deepak Kumar <dk1609008@gmail.com>
2020-04-17 22:52:59 +0530

    dashboard complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import { AngularDropdownModule } from 'angular-dropdown';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -24,7 +26,10 @@ import { LifetimeMembersComponent } from './lifetime-members/lifetime-members.co
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ChartsModule
+    // AngularDropdownModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType, ChartOptions } from 'chart.js';
+import { SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip } from 'ng2-charts';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public pieChartLabels: Label[] = ['Students', 'volunteers', 'Supporters', 'Donors'];
+  public pieChartData: SingleDataSet = [500, 200, 100, 300];
+  public pieChartType: ChartType = 'pie';
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
 
-  ngOnInit(): void {
-  }
+constructor() {
+  monkeyPatchChartJsTooltip();
+  monkeyPatchChartJsLegend();
+}
+
+ngOnInit() {
+}
 
 }
